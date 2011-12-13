743674f943a8e6019fd29e2ccb8f316aa5293942
Matej Kramny <matejkramny@gmail.com>
2011-12-13 14:54:24 +0300

    Added beginning of Forms generator

--- inc/Module.php
+++ inc/Module.php
@@ -4,6 +4,37 @@
 
 abstract class Module
 {
-	abstract function load ($name);
-	
+}
+
+final class Form
+{
+	// Contructor
+	public function Form ($a)
+	{
+		/*
+		 * $a = array (
+		 *     'action' => 'some url', // optional, default is the same page
+		 *     'method' => 'post or get', // optional, default is POST
+		 *     'enctype' => 'encoding type', // optional
+		 *     'data' => array (
+		 *         'somelabel' => array (
+		 *             'type' => 'datatype',
+		 *             'label' => 'the information before the input, like "Username:"'
+		 *             'name' => 'name of the element, to be accessed as $_GET/POST["yournameofelement"]'
+		 *             // more optional parameters based on the 'type'
+		 *         )
+		 *     )
+		 * )
+		 * 
+		 * Data types:
+		 * 	- datatype { // some relevant parameters (value type, like string, bool) }
+		 * 	- input { required (bool), value (string), validator (array ( length (int), regex (string), error (string) )) }
+		 *	- password { required (bool), value (string), validator (array ( length (int), regex (string), error (string) )) }
+		 *	- file { required (bool), location (string) }
+		 *	- radio ( required (bool) }
+		 *	- checkbox ( required (bool) }
+		 *	- custom { content (string) }
+		 */
+
+	}
 }
