c70a838cb433a4fc645526d44918dd945f3292ef
Matej Kramny <matejkramny@gmail.com>
2012-01-09 15:59:55 +0000

    MySQL Dump

--- framework.sql
+++ framework.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 06, 2012 at 11:46 PM
+-- Generation Time: Jan 09, 2012 at 03:59 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.2
 
@@ -12,9 +12,29 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `framework`
 --
-DROP DATABASE `framework`;
 CREATE DATABASE `framework` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `framework`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_uploads`
+--
+
+DROP TABLE IF EXISTS `file_uploads`;
+CREATE TABLE IF NOT EXISTS `file_uploads` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `location` varchar(256) NOT NULL,
+  `filename` varchar(256) NOT NULL,
+  `extension` varchar(256) NOT NULL,
+  `timestamp` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `file_uploads`
+--
+
 
 -- --------------------------------------------------------
 
@@ -26,6 +46,7 @@ DROP TABLE IF EXISTS `forms`;
 CREATE TABLE IF NOT EXISTS `forms` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `code` varchar(256) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL COMMENT 'Unix timestamp',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -61,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `profiles` (
 -- Dumping data for table `profiles`
 --
 
-INSERT INTO `profiles` (`id`, `name`, `surname`, `username`, `email`, `salt`, `password`, `lang`, `template`, `created`, `last_access`, `last_ip`) VALUES(3, 'matej', 'kramny', 'mkram0', 'matejkramny@gmail.com', '63c7727ac872c8e3', '6kBQL1VzVnJQOfEmozDEByJFZv557oJwF6TAKXHQqNY5VSITHESmTp6zJcaZR9mzmA4DhZBiNjUB.qZdmEPlv0', 'en', 1, 1323521498, 0, '');
+INSERT INTO `profiles` VALUES(3, 'matej', 'kramny', 'mkram0', 'matejkramny@gmail.com', '63c7727ac872c8e3', '6kBQL1VzVnJQOfEmozDEByJFZv557oJwF6TAKXHQqNY5VSITHESmTp6zJcaZR9mzmA4DhZBiNjUB.qZdmEPlv0', 'en', 1, 1323521498, 0, '');
 
 -- --------------------------------------------------------
 
@@ -101,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `sys_languages` (
 -- Dumping data for table `sys_languages`
 --
 
-INSERT INTO `sys_languages` (`id`, `lang_code`, `lang_full`) VALUES(1, 'en', 'English');
+INSERT INTO `sys_languages` VALUES(1, 'en', 'English');
 
 -- --------------------------------------------------------
 
@@ -115,15 +136,16 @@ CREATE TABLE IF NOT EXISTS `sys_settings` (
   `name` varchar(256) NOT NULL,
   `value` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `sys_settings`
 --
 
-INSERT INTO `sys_settings` (`id`, `name`, `value`) VALUES(1, 'lang', 'en');
-INSERT INTO `sys_settings` (`id`, `name`, `value`) VALUES(2, 'template', '1');
-INSERT INTO `sys_settings` (`id`, `name`, `value`) VALUES(3, 'template_path', 'default');
+INSERT INTO `sys_settings` VALUES(1, 'lang', 'en');
+INSERT INTO `sys_settings` VALUES(2, 'template', '1');
+INSERT INTO `sys_settings` VALUES(3, 'template_path', 'default');
+INSERT INTO `sys_settings` VALUES(4, 'upload_file_size', '4096');
 
 -- --------------------------------------------------------
 
@@ -143,4 +165,26 @@ CREATE TABLE IF NOT EXISTS `sys_templates` (
 -- Dumping data for table `sys_templates`
 --
 
-INSERT INTO `sys_templates` (`id`, `name`, `path`) VALUES(1, 'Default look', 'default');
+INSERT INTO `sys_templates` VALUES(1, 'Default look', 'default');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testTable`
+--
+
+DROP TABLE IF EXISTS `testTable`;
+CREATE TABLE IF NOT EXISTS `testTable` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `gender` varchar(256) NOT NULL,
+  `picture` int(11) NOT NULL,
+  `terms` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `testTable`
+--
+
